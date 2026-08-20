f761e7b234924d8f11b22d2a3fb2097b356ef62d
zinoo <77845029+zinoo-dez@users.noreply.github.com>
2026-08-20 15:02:38 +0630

    chore(config): update canonical site URL to https://aws01.vercel.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://aws-guild.dev',
+  site: 'https://aws01.vercel.app',
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
